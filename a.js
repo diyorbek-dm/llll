6d7093c18f02d4200d70558e6ef2efb27a5c937d
diyorbek-dm <123005290+diyorbek-dm@users.noreply.github.com>
2023-01-22 12:22:51 +0500

    Add files via upload

--- a.js
+++ a.js
@@ -1,13 +1,2 @@
-let a = +prompt('Введите число (1): ')
-let b = +prompt('Введите число (2): ')
-let d = +prompt('Введите число (3): ')
+'use strict'
 
-if(a < b && a > d || a > b && a < d) {
-  alert('Среднее число из выражения: ' + a)
-} else if(b < a && b > d || b > a && b < d) {
-  alert('Среднее число из выражения: ' + b)
-} else if(d > a && d < b || d < a && d > b) {
-  alert('Среднее число из выражения: ' + d)
-} else {
-  alert('Ошибка!')
-}
